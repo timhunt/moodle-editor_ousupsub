36158856670f60b21128118cdf857295b002477e
Tim Hunt <T.J.Hunt@open.ac.uk>
2015-04-29 16:26:31 +0100

    Remove some debug code I accidentally committed.

--- tests/fixtures/editortestpage.php
+++ tests/fixtures/editortestpage.php
@@ -41,37 +41,9 @@ $PAGE->set_heading('Test superscript/subscript editor');
 
 echo $OUTPUT->header();
 
-echo html_writer::label('New input:', 'supsub') . ' ';
-echo html_writer::tag('textarea', 'Normal<sup>sup</sup><sub>sub</sub>',
-        array('name' => 'supsub', 'id' => 'supsub', 'rows' => 2, 'cols' => 20));
+echo html_writer::label('Input', 'supsub');
+echo html_writer::tag('textarea', '', array('name' => 'supsub', 'id' => 'supsub', 'rows' => 2, 'cols' => 20));
 $editor->use_editor('supsub', array('supsub' => $type));
-
-echo '<p><label for="textarea">Plain input</label> <textarea id="textarea" rows="2" cols="20">Normal</textarea></p>';
-
-echo '<p><label for="plain">Plain input</label> <input id="plain" type="text" value="Normal" size="20"></p>';
-
-$editor = get_texteditor('supsub');
-echo html_writer::label('Old input', 'oldsupsub') . ' ';
-echo html_writer::tag('textarea', 'Normal<sup>sup</sup><sub>sub</sub>',
-        array('name' => 'oldsupsub', 'id' => 'oldsupsub', 'rows' => 2, 'cols' => 20));
-$editor->use_editor('oldsupsub', array('supsub' => $type));
-
-
-$editor = get_texteditor('ousupsub');
-echo html_writer::label('New input:', 'supsub40') . ' ';
-echo html_writer::tag('textarea', 'Normal<sup>sup</sup><sub>sub</sub>',
-        array('name' => 'supsub40', 'id' => 'supsub40', 'rows' => 2, 'cols' => 40));
-$editor->use_editor('supsub40', array('supsub' => $type));
-
-echo '<p><label for="textarea">Plain input</label> <textarea id="textarea" rows="2" cols="40">Normal</textarea></p>';
-
-echo '<p><label for="plain">Plain input</label> <input id="plain" type="text" value="Normal" size="40"></p>';
-
-$editor = get_texteditor('supsub');
-echo html_writer::label('Old input', 'oldsupsub40') . ' ';
-echo html_writer::tag('textarea', 'Normal<sup>sup</sup><sub>sub</sub>',
-        array('name' => 'oldsupsub40', 'id' => 'oldsupsub40', 'rows' => 2, 'cols' => 40));
-$editor->use_editor('oldsupsub40', array('supsub' => $type));
 
 echo $OUTPUT->footer();
 
