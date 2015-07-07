a3635a30cfadae6e02b0f17f21de883631319141
Colin Chambers <c.chambers@open.ac.uk>
2015-07-07 11:46:13 +0100

    fixed Toolbar not accessible via keyboard #86947

--- yui/build/moodle-editor_ousupsub-editor/moodle-editor_ousupsub-editor-debug.js
+++ yui/build/moodle-editor_ousupsub-editor/moodle-editor_ousupsub-editor-debug.js
@@ -160,6 +160,16 @@ Y.extend(Editor, Y.Base, {
      */
     _eventHandles: null,
 
+    /**
+     * The current focal point for tabbing.
+     *
+     * @property _tabFocus
+     * @type Node
+     * @default null
+     * @private
+     */
+    _tabFocus: null,
+
     initializer: function() {
         // Note - it is not safe to use a CSS selector like '#' + elementid because the id
         // may have colons in it - e.g.  quiz.
@@ -451,6 +461,142 @@ Y.extend(Editor, Y.Base, {
         if (this.textareaLabel) {
             this.toolbar.setAttribute('aria-labelledby', this.textareaLabel.get("id"));
         }
+
+        // Add keyboard navigation for the toolbar.
+        this.setupToolbarNavigation();
+
+        return this;
+    },
+
+    /**
+     * Set up the watchers for toolbar navigation.
+     *
+     * @method setupToolbarNavigation
+     * @chainable
+     */
+    setupToolbarNavigation: function() {
+        // Listen for Arrow left and Arrow right keys.
+        this._wrapper.delegate('key',
+                this.toolbarKeyboardNavigation,
+                'down:37,39',
+                '.' + CSS.TOOLBAR,
+                this);
+        this._wrapper.delegate('focus',
+                function(e) {
+                    this._setTabFocus(e.currentTarget);
+                }, '.' + CSS.TOOLBAR + ' button', this);
+
+        return this;
+    },
+
+    /**
+    * Implement arrow key navigation for the buttons in the toolbar.
+    *
+    * @method toolbarKeyboardNavigation
+    * @param {EventFacade} e - the keyboard event.
+    */
+   toolbarKeyboardNavigation: function(e) {
+       // Prevent the default browser behaviour.
+       e.preventDefault();
+
+       // On cursor moves we loops through the buttons.
+       var buttons = this.toolbar.all('button'),
+           direction = 1,
+           button,
+           current = e.target.ancestor('button', true);
+
+       if (e.keyCode === 37) {
+           // Moving left so reverse the direction.
+           direction = -1;
+       }
+
+       button = this._findFirstFocusable(buttons, current, direction);
+       if (button) {
+           button.focus();
+           this._setTabFocus(button);
+       } else {
+           Y.log("Unable to find a button to focus on", 'debug', LOGNAME);
+       }
+   },
+
+   /**
+    * Find the first focusable button.
+    *
+    * @param {NodeList} buttons A list of nodes.
+    * @param {Node} startAt The node in the list to start the search from.
+    * @param {Number} direction The direction in which to search (1 or -1).
+    * @return {Node | Undefined} The Node or undefined.
+    * @method _findFirstFocusable
+    * @private
+    */
+   _findFirstFocusable: function(buttons, startAt, direction) {
+       var checkCount = 0,
+           group,
+           candidate,
+           button,
+           index;
+
+       // Determine which button to start the search from.
+       index = buttons.indexOf(startAt);
+       if (index < -1) {
+           Y.log("Unable to find the button in the list of buttons", 'debug', LOGNAME);
+           index = 0;
+       }
+
+       // Try to find the next.
+       while (checkCount < buttons.size()) {
+           index += direction;
+           if (index < 0) {
+               index = buttons.size() - 1;
+           } else if (index >= buttons.size()) {
+               // Handle wrapping.
+               index = 0;
+           }
+
+           candidate = buttons.item(index);
+
+           // Add a counter to ensure we don't get stuck in a loop if there's only one visible menu item.
+           checkCount++;
+
+           // Loop while:
+           // * we haven't checked every button;
+           // * the button is hidden or disabled;
+           // * the group is hidden.
+           if (candidate.hasAttribute('hidden') || candidate.hasAttribute('disabled')) {
+               continue;
+           }
+           group = candidate.ancestor('.ousupsub_group');
+           if (group.hasAttribute('hidden')) {
+               continue;
+           }
+
+           button = candidate;
+           break;
+       }
+
+       return button;
+    },
+
+    /**
+     * Sets tab focus for the toolbar to the specified Node.
+     *
+     * @method _setTabFocus
+     * @param {Node} button The node that focus should now be set to
+     * @chainable
+     * @private
+     */
+    _setTabFocus: function(button) {
+        if (this._tabFocus) {
+            // Unset the previous entry.
+            this._tabFocus.setAttribute('tabindex', '-1');
+        }
+
+        // Set up the new entry.
+        this._tabFocus = button;
+        this._tabFocus.setAttribute('tabindex', 0);
+
+        // And update the activedescendant to point at the currently selected button.
+        this.toolbar.setAttribute('aria-activedescendant', this._tabFocus.generateID());
 
         return this;
     },
